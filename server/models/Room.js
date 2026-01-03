553b69bb58fb79710a72ed73a330b7ebee9b00b2
張乃云 <clarice17009@gmail.com>
2026-01-03 21:33:56 +0800

    Update

--- server/models/Room.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const roomSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }, // 房間名稱
-  password: { type: String, default: null },            // 房間密碼
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Room', roomSchema);
